d97d49507296f3d97b0df4ee7432b5e6f2d52b84
Adeel-Munir <111064925+Adeelmunir707@users.noreply.github.com>
2026-03-13 15:42:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -60,22 +60,31 @@ st.markdown(html_temp, unsafe_allow_html=True)
 input_sms = st.text_area("Enter the message")
 
 if st.button('Inquire'):
-
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
     # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+    # 3. predict probability
+    prob = model.predict_proba(vector_input)[0]
+    spam_prob = prob[1] * 100
+    ham_prob = prob[0] * 100
+
+
+    # Optional: display final label
     result = model.predict(vector_input)[0]
-    # 4. Display
     if result == 1:
         st.header("Spam")
     else:
         st.header("Not Spam")
+    
+     # 4. Display the probabilities
+    st.write(f"Spam Probability: {spam_prob:.2f}%")
+    st.write(f"Not Spam Probability: {ham_prob:.2f}%")
 
 st.write("\n\n\n\n\n")
 
-st.write("")
-st.write("")
-st.write("")
-st.write("\t\t\t\t\t\tCopy© 2026 Adeel Munir")
+st.write("\n" * 15)
+# Add a bold line above the footer
+st.markdown("<hr style='border: 2px solid black;'>", unsafe_allow_html=True)
+# Footer content
+st.write("Copy© 2026 Adeel Munir | Made With ❤️ in Pakistan")
